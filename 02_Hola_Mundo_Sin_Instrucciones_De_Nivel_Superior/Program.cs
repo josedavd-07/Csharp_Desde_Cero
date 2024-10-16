aedd5393b760ab43ea3b3df13552539d6592d651
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2024-10-15 23:10:36 -0500

    Agrgamos la forma de hacer un hola mundo en C# sin hacer uso de instrucciones superiores.

---
+++ 02_Hola_Mundo_Sin_Instrucciones_De_Nivel_Superior/Program.cs
@@ -0,0 +1,15 @@
+﻿using System; //Uso del sistema de bibliotecas de C#
+
+namespace _02_Hola_Mundo_Sin_Instrucciones_De_Nivel_Superior//Nombre del espacio de nombres del proyecto 
+{
+    internal class Program //Clase principal del proyecto
+    {
+        static void Main(string[] args)  //Método principal del proyecto de ejecución
+        {
+            Console.WriteLine("Hello, World!");  //Impresión de texto en consola
+
+            Console.ReadKey(); //Espera a que el usuario presione una tecla para cerrar la consola
+        }
+    }
+}
+
