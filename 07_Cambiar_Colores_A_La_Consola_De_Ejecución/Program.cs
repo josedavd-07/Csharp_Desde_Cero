faebae41a59a8e3c9dafe9e353caf69d4702805e
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2024-10-16 15:48:18 -0500

    Añadimos la sección de  cambios de colores en la consola  de texto y de fondo y como trabajarlas

---
+++ 07_Cambiar_Colores_A_La_Consola_De_Ejecución/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _07_Cambiar_Colores_A_La_Consola_De_Ejecución
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Cambiar colores de la consola";
+
+            //Cambiar el color del texto de la consola
+            Console.WriteLine("Aca cambiamos el color del texto de la consola.");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nEste texto es de color verde");
+            Console.ResetColor();
+
+            Console.WriteLine("\npresiona cualquiera tecla  para continuar con la ejecución del programa");
+            Console.ReadKey();
+            Console.Clear();
+
+            //Cambiar el color del fondo de la consola
+            string mensaje = ("Aca cambiamos el color del fondo de la consola a blanco y este texto es Magenta" +
+                " pero lo veras en la siguiente ejecución , mientras tanto este texto sera magenta");
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"{mensaje} y todo en conjunto se vera en la siguiente ejecución. ");
+            Console.ResetColor ();
+
+            Console.WriteLine("\npresiona cualquiera tecla  para continuar con la ejecución del programa");
+            Console.ReadKey();
+            Console.Clear();
+
+            //Cambio de fondo con texto completo.
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"{mensaje} y ahora junto con el fondo blanco pero aun falta" +
+                $" que todo sea blanco en la consola y lo veremos en la siguiente ejecucion");
+            Console.ResetColor();
+
+            Console.WriteLine("\npresiona cualquiera tecla  para continuar con la ejecución del programa");
+
+            Console.ReadKey();
+            Console.Clear();
+
+            //Cambio de fondo con texto completo.
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"{mensaje} y ahora junto con el fondo  todo balnco y el texto correspondiente en magenta");
+
+            Console.WriteLine("\n\npresiona cualquiera tecla  para continuar con la ejecución del programa");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.ResetColor();
+            Console.Clear();
+
+            Console.WriteLine("Fin del programa, espero que te sirva este recurson y te sea de gran ayuda," +
+                " presiona cualquiera tecla antes de salir, y sigue programando que este camino es maravilloso");
+            Console.ReadKey ();
+        }
+    }
+}
