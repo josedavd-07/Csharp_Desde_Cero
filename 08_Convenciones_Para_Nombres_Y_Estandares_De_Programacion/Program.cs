962f19a2f19abb7b74d5c2b8980cda88c083781e
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2024-10-16 17:54:21 -0500

    Añadimos las secciones de codigo de la clase N° 8

---
+++ 08_Convenciones_Para_Nombres_Y_Estandares_De_Programacion/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+
+namespace _08_Convenciones_Para_Nombres_Y_Estandares_De_Programacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Convenciones de escritura existentes
+
+            //camelCase 
+            //Ejemplo: nombreDeVariable
+
+            //Pascal Case
+            //Ejemplo: NombreDeVariable
+
+            //Snake Case
+            //Ejemplo: nombre_de_variable
+
+
+            /*Las usadas en C# son:*/
+
+            //PascalCase
+            //camelCase
+
+            //**************************
+
+            //Para las clases utilizaremos PascalCase
+
+            //Ejemplo:
+            /*
+             
+              public class persona
+              {
+                  //Codigo de la clase
+              }
+
+             */
+
+            //**************************
+
+            //Para las variables utilizaremos camelCase
+
+            //Ejemplo:
+            /*
+             
+              int edadPersona = 20;
+
+             */
+
+            //**************************
+
+            // para los metodos utilizaremos camelCase
+
+            //Ejemplo:
+
+            /*
+                public void mostrarMensaje()
+                {
+                
+                }
+             
+             */
+
+            //**************************
+
+            //No utilizar numeros al inicio de los nombres de las variables
+
+            //Ejemplo:
+
+            /*
+             
+              int 1numero = 10;
+
+             */
+
+            //Forma correcta de nombrar la variable
+
+            //Ejemplo:
+
+            /*
+             
+            
+            int numero1 = 10;
+
+             */
+
+            //**************************
+
+            //No utilizar guiones al inicio de los nombres de las variables
+
+            //Ejemplo:
+
+            /*
+             
+              int -numero = 10;
+
+             */
+
+            //Forma correcta de nombrar la variable es con guion bajo en algunos caso se suele utilizar
+            //para campos privados de una clase
+
+            //Ejemplo:
+
+            /*
+             
+              private int _numero = 10;
+
+             */
+
+            //**************************
+
+            //No utlizar notacion hungara
+
+            //Ejemplo:
+
+            /*
+             
+              int ctrUsers;
+
+             */
+
+            //Forma correcta de nombrar la variable
+
+            //Ejemplo:
+
+            /*
+             
+              int controlUsuarios;
+
+             */
+
+            //**************************
+
+            //No utilizar nombres de variables que sean palabras reservadas del lenguaje
+
+            //Ejemplo:
+
+            /*
+             
+              int int = 10;
+
+             */
+
+            //Forma correcta de nombrar la variable
+
+            //Ejemplo:
+
+            /*
+             
+              int numero = 10;
+
+             */
+
+            //**************************
+
+            //Esto es tod espero que te haya sido de gran ayuda y nos vemos en la siguiente clase sigue aprendiendo
+        }
+    }
+}
