dce6069280ee4aa5eba55d01bb72dc27de9df2b1
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2024-10-15 22:38:37 -0500

    Se acaba de publicar un hola mundo en C# con instrucciones de nivle superior

--- 01_Hola_Mundo_Usando_Instrucciones_De_Nivel_Superior/Program.cs
+++ 01_Hola_Mundo_Usando_Instrucciones_De_Nivel_Superior/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //forma de crear un hola mundo en C#
-//usando instrucciones de nivel superior
+//usando instrucciones de nivel superior en C#
 Console.WriteLine("Hello, World!");
