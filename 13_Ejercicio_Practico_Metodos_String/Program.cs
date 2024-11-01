a3fb98aa2406973329998194128efaf59d6a8db9
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2024-10-31 22:24:53 -0500

    Añadimos el ejercicio N° de los metodos de String

---
+++ 13_Ejercicio_Practico_Metodos_String/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _13_Ejercicio_Practico_Metodos_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Ejercicio  N° 1 de metodos de string");
+            Console.ResetColor();
+
+            Console.Write("\nPor favor ingrese un texto: ");
+            string ingreso = Console.ReadLine();
+            Console.Write("\nIngrese el caracter que quiere buscar en el texto: ");
+            char ingresoBuscado = Console.ReadLine()[0];
+            int buscarIndice = ingreso.IndexOf(ingresoBuscado);
+            Console.WriteLine("\nEl índice del caracter {0} en el texto es: {1}", ingresoBuscado, buscarIndice);
+            
+            Console.WriteLine("\nPor favor presione culquiera tecla para continuar con el segundo ejercicio");
+            Console.ReadKey();
+            Console.Clear();
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nEjercicio  N° 2 de metodos de string");
+            Console.ResetColor();
+
+            Console.Write("\nPor favor ingrese su nombre: ");
+            string nombre = Console.ReadLine();
+            Console.Write("\nPor favor ingrese su apellido: ");
+            string apellido = Console.ReadLine();
+            string nombrecompleto = String.Concat(nombre, " ", apellido);
+            Console.WriteLine("\nEl nombre completo es: {0}", nombrecompleto);
+
+            Console.WriteLine("\nPor favor presione cualquiera tecla pra terminar la ejecución del programa");
+            Console.ReadKey();
+        }
+    }
+}
