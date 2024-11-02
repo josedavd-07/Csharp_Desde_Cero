78508530641937697c78e3a4d298186a90ef13e5
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2024-11-01 21:46:05 -0500

    agragamos la clase N° 15 sobre constantes

---
+++ 15_Constantes/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _15_Constantes
+{
+    internal class Program
+    {
+        //Constantes declaradas de forma global
+        //Los valores de las constantes no pueden cambiar su valor en tiempo de ejecución.
+        //Ya que estos son valores ya definidos en si que su valor no de be y no se puede cambiar
+        //Por ejemplo  el numero PI, Nuestra fecha de nacimiento y demas.
+
+        const double PI = 3.14159265359;
+        const int semanas = 52, meses = 12;
+        const string nacimiento = "07/02/2005";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi nacimiento fue el: {0}", nacimiento);
+            Console.Read();
+        }
+  
+    }
+}
