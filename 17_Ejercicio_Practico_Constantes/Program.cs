0b5f9ca5c1505b5b09db7285f2c577156abd992f
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2024-11-01 22:10:58 -0500

    Agregamos otro ejercicio de practicas de constantes

---
+++ 17_Ejercicio_Practico_Constantes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _17_Ejercicio_Practico_Constantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                ** Declara una constante de  tipo entero con el nombre colorFabrica
+                * y asigna el valor de fabrica de color que lleve algun vehiculo que tengas.
+                * y muestra su resultado en la consola con un metodo de salida.
+            */
+
+            const string colorFabrica = "Verde";
+            Console.WriteLine("¿De que color fu el color de fabrica de su vehiculo: ?");
+            Console.Write($"\nEl color de fabrica de mi vehiculo fue: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(colorFabrica);
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
