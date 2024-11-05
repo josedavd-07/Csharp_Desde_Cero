4ba613e88b492718b09f3e2f354ff46738e5595b
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2024-11-05 14:43:21 -0500

    Corregimos le metodo añadiendo otra seccion mas de ejerccios

--- 20_Ejercicio_Practico_Void/Program.cs
+++ 20_Ejercicio_Practico_Void/Program.cs
@@ -8,18 +8,32 @@ namespace _20_Ejercicio_Practico_Void
         {
             /*
                 * Crea un método de tipo void que se llame Animales() ->
-                * dentro del cual se debe imprimir en consola el siguiente mensaje: Hola. Habla el elefante ->
+                * dentro del cual se debe imprimir en consola el siguiente mensaje: Hola. Habla el elefante desde un metodo sin parametros ->
                 * Recuerda luego agregar el llamado al nuevo método, dentro del método Main.
             */
 
             Animales();
+            AnimalesUsandoParametros("Hola. Habla el elefante desde un metodo con parametros");
             Console.ReadKey();
         }
 
         //Declarracion del metodo
         public static void Animales()
         {
-            Console.WriteLine("Hola. Habla el elefante");
+            Console.WriteLine("Hola. Habla el elefante desde un metodo sin parametros");
+        }
+
+
+        /*
+            * Crea el mismo método que en el ejercicio previo, -> 
+            * pero en lugar de que el mensaje se escriba dentro del método AnimalesUsandoParametros() -> 
+            * el mismo debe enviarse como parámetro o argumento del método en el momento -> 
+            * de hacer el llamado desde el método Main. El mensaje debe ser el mismo pero en vez de sin parametro con parametros 
+        */
+
+        public static void AnimalesUsandoParametros(string animales)
+        {
+            Console.WriteLine($"\n{animales}");
         }
     }
 }
