2f2ffb1c5b45f812001459d9e2041e921963d36a
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2024-11-05 14:02:44 -0500

    Añadimos lo basico para comenzar con los metodos

---
+++ 19_Metodo_Void/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _19_Metodo_Void
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Método void sin parametro.
+            EscribeAlgo();
+
+            // Método void con parametro.
+            EscribeAlgoEspecifico("Soy un argumento y me llamaron desde un método, por medio  de un " +
+                "parametro que fui asignado");
+
+
+            Console.ReadKey();
+        }
+
+        // NivelDeAcceso - (static) - TipoDeRetorno - NombreDelMétodo (Parámetro1, Parámetro2)
+        public static void EscribeAlgo()
+        {
+            Console.WriteLine("Yo nací en un método el cual se llama 'EscribeAlgo' y soy publico de tipo" +
+                " estatico yu no retorno datos");
+        }
+
+        public static void EscribeAlgoEspecifico(string miTexto)
+        {
+            Console.WriteLine($"\n{miTexto}");
+        }
+    }
+}
