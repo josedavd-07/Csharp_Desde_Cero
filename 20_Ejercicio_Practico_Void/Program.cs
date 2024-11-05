bb5463e0c2f413d689fe5613fc82179d4e51e9b6
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2024-11-05 14:16:47 -0500

    Ejercicio practico de metodos void

---
+++ 20_Ejercicio_Practico_Void/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _20_Ejercicio_Practico_Void
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                * Crea un método de tipo void que se llame Animales() ->
+                * dentro del cual se debe imprimir en consola el siguiente mensaje: Hola. Habla el elefante ->
+                * Recuerda luego agregar el llamado al nuevo método, dentro del método Main.
+            */
+
+            Animales();
+            Console.ReadKey();
+        }
+
+        //Declarracion del metodo
+        public static void Animales()
+        {
+            Console.WriteLine("Hola. Habla el elefante");
+        }
+    }
+}
