aa31efaf4ba0d183dcd0d6b3990c921b0d4b4d78
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2024-11-06 18:26:03 -0500

    Agregamos la toma de decisiones, la sentencia if

---
+++ 28_Tomas_de_Decisiones_If/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _28_Tomas_de_Decisiones_If
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pedimnos informacion al cliente
+            Console.WriteLine("Por favor, ingresa la temperatura actual");
+
+            string temperatura = Console.ReadLine();
+            int numTemperatura = int.Parse(temperatura);
+
+            //Solo usamos sentencias de if para este ejercicio luego mas adelante veremos los else if y else.
+
+            if (numTemperatura < 20)
+            {
+                Console.Write("Abrígate!");
+            }
+            if (numTemperatura == 20)
+            {
+                Console.WriteLine("Vístete cómodo");
+            }
+            if (numTemperatura > 20)
+            {
+                Console.WriteLine("Usa ropa bien liviana");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
