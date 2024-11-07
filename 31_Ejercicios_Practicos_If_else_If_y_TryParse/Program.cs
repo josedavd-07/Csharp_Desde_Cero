0382721b5aeee2eb79ff5a5bfde49bcb8e391936
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2024-11-06 19:38:07 -0500

    añadimos  los ejercicios de la clase  31

---
+++ 31_Ejercicios_Practicos_If_else_If_y_TryParse/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _31_Ejercicios_Practicos_If_else_If_y_TryParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                * Utilizando el método TryParse, intenta convertir el string "edad" en un int.
+                * 
+                * - En caso de que la conversión sea posible, se debe imprimir en consola 
+                *   el mensaje "Número válido".
+                * 
+                * - En caso contrario, se debe imprimir el mensaje "Número no válido".
+            */
+
+            string edad = "19";
+            int numero;
+
+            if (int.TryParse(edad, out numero))
+            {
+                Console.WriteLine("Número valido");
+            }
+            else
+            {
+                Console.WriteLine("Número no valido");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
