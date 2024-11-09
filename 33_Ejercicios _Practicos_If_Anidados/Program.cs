9a30bdd7602d00f19f8fe923b6d586a65b5c234e
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2024-11-08 23:06:32 -0500

    ejercicios de if practicos agregados

---
+++ 33_Ejercicios _Practicos_If_Anidados/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace _33_Ejercicios__Practicos_If_Anidados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                * EJERCICIO NUMERO 1
+                * 
+                * UTILIZANDO IF ANIDADOS, SE DEBE CREAR UNA ESTRUCTURA QUE CUMPLA 
+                * CON LA SIGUIENTE LÓGICA:
+                * 
+                * - Si el valor de resultadoExamen es menor a 4, se debe imprimir 
+                *   en consola el mensaje "Aplazado".
+                * 
+                * - Si el valor de resultadoExamen es mayor o igual a 4 pero menor a 7, 
+                *   se debe imprimir en consola el mensaje "Aprobado".
+                * 
+                * - Si el valor de resultadoExamen es mayor o igual a 7, se debe imprimir 
+                *   en consola el mensaje "Promocionado".
+            */
+
+            int resultadoExamen = 8;
+
+            if (resultadoExamen < 4)
+            {
+                Console.WriteLine("Aplazado");
+            }
+            else
+            {
+                if (resultadoExamen < 7)
+                {
+                    Console.WriteLine("Aprobado");
+                }
+                else
+                {
+                    Console.WriteLine("Promocionado");
+                }
+            }
+             
+            Console.WriteLine("\n\n");
+
+            /*
+                * EJERCICIO NUMERO 2
+                * 
+                * DADAS LAS DOS VARIABLES BOOLEANAS, DECLARAR UN IF COMPUESTO CON 
+                * DICHAS DOS VARIABLES. 
+                * 
+                * ES DECIR, UTILIZANDO LOS OPERADORES CONDICIONALES, SE DEBE 
+                * VERIFICAR QUE AMBAS CONDICIONES SEAN VERDADERAS (&&).
+                * 
+                * - Si la condición total es verdadera, se debe imprimir en consola 
+                *   el mensaje "La condición es verdadera".
+                * 
+                * - En caso contrario, se debe imprimir en consola el mensaje 
+                *   "La condición es falsa".
+            */
+
+            bool esMayor = 5 > 7;
+            bool condicion = true;
+
+            if (esMayor && condicion)
+            {
+                Console.WriteLine("La condicion es verdadera");
+            }
+            else
+            {
+                Console.WriteLine("La condicion es falsa");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 34_Desafio_Declaraciones_If/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace _34_Desafio_Declaraciones_If
+{
+    internal class Program
+    {
+        //Variables globales
+        static string nombreDeUsuario;
+        static string clave;
+        
+
+        static void Main(string[] args)
+        {
+            /*
+                * CREA UN SISTEMA DE INICIO DE SESIÓN DE USUARIO, DONDE EL USUARIO 
+                * PUEDE PRIMERO REGISTRARSE Y LUEGO INICIAR SESIÓN.
+                * 
+                * EL PROGRAMA DEBE VERIFICAR SI EL USUARIO HA INGRESADO EL NOMBRE 
+                * DE USUARIO Y LA CONTRASEÑA CORRECTOS AL INICIAR SESIÓN, 
+                * (ES DECIR, LOS MISMOS QUE UTILIZÓ AL REGISTRARSE).
+            */
+
+            Registro();
+            Ingreso();
+            Console.ReadKey();  
+        }
+
+        //Metodos
+        public static void Registro()
+        {
+            Console.WriteLine("Por favor ingrese su nombre de usuario");
+            nombreDeUsuario = Console.ReadLine();
+            Console.WriteLine("Por favor ingrese su claveo");
+            clave = Console.ReadLine();
+            Console.WriteLine("Registro exitoso");
+            Console.WriteLine("--------------------");
+        }
+
+        public static void Ingreso()
+        {
+            Console.WriteLine("Por favor ingrese su nombre de usuario");
+            if (nombreDeUsuario == Console.ReadLine())
+            {
+                Console.WriteLine("Por favor ingrese su clave");
+                if (clave == Console.ReadLine())
+                {
+                    Console.WriteLine("Ingreso exitoso!");
+                }
+                else
+                {
+                    Console.WriteLine("Clave incorrecta, por favor reinicie el programa");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Nombre de usuario incorrecto, por favor reinicie el programa");
+            }
+        }
+    }
+}
