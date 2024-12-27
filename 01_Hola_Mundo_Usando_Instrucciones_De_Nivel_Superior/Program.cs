697e9efd3e2cb4e25847b1e0da20cc11db5d0a0e
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2024-12-26 22:57:06 -0500

    Añadi una seccion de codigo  a la clase numero 1.

--- 01_Hola_Mundo_Usando_Instrucciones_De_Nivel_Superior/Program.cs
+++ 01_Hola_Mundo_Usando_Instrucciones_De_Nivel_Superior/Program.cs
@@ -2,3 +2,5 @@
 //forma de crear un hola mundo en C#
 //usando instrucciones de nivel superior en C#
 Console.WriteLine("Hello, World!");
+Console.WriteLine("\n Este mensaje es para recordarte que  fue escrito este programa" +
+    " usando las instrucciones de nivel superior");
