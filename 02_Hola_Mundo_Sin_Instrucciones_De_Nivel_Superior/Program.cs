fb9506ccaf938aec48f6e4c685d4a4ea1a3f671c
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2024-12-26 23:04:29 -0500

    Añadi una seccion de codigo  sin hacer uso de las instrucciones superiores

--- 02_Hola_Mundo_Sin_Instrucciones_De_Nivel_Superior/Program.cs
+++ 02_Hola_Mundo_Sin_Instrucciones_De_Nivel_Superior/Program.cs
@@ -7,6 +7,8 @@ namespace _02_Hola_Mundo_Sin_Instrucciones_De_Nivel_Superior//Nombre del espacio
         static void Main(string[] args)  //Método principal del proyecto de ejecución
         {
             Console.WriteLine("Hello, World!");  //Impresión de texto en consola
+            Console.WriteLine("\n Este mensaje es para decirte que este mensaje fue" +
+                " escrito sin usar las instrucciones de nivel superior en C#"); //Impresión de texto en consola
 
             Console.ReadKey(); //Espera a que el usuario presione una tecla para cerrar la consola
         }
