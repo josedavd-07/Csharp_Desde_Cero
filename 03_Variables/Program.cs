ec69c4e6880c21fb5697754814520f26b6981f03
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2024-12-26 23:47:10 -0500

    Añadimos secciones de codigo nuevas.

--- 03_Variables/Program.cs
+++ 03_Variables/Program.cs
@@ -19,9 +19,11 @@ namespace _03_Variables
 
             //Variable de tipo entero inicializada con el valor 10
             int numero = 10;
+            int notaFinalCurso = 500; //Variable de tipo entero inicializada con el valor 500
 
             //Variable de tipo flotante inicializada con el valor 10.5
             float numeroFlotante = 10.5f;
+            float numeroNotaFinalFlotante = 2.9f;
 
             //Variable sin inicializar que luego de cambiara su valor
             int numero2;
@@ -31,6 +33,9 @@ namespace _03_Variables
 
             Console.WriteLine("El valor de la variable numero es: " + numero);
             Console.WriteLine("\nEl valor de la variable numeroFlotante es: " + numeroFlotante);
+            Console.WriteLine("\nla nota final del curso es : " + notaFinalCurso);
+            Console.WriteLine("\nla nota final del curso en flotante es : " + numeroNotaFinalFlotante);
+
 
             //Asignamos un nuevo valor a la variable numero
             Console.Write("\nIngrese un nuevo valor para la variable numero: ");
