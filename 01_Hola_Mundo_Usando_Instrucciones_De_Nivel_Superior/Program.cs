15f7ed6467ba9b7d391989b0f4d294ca60029f43
Jose David Carranza Angarita <ing.josedavidcarranzaangarita@gmail.com>
2025-02-15 00:55:21 -0500

    update information for script

--- 01_Hola_Mundo_Usando_Instrucciones_De_Nivel_Superior/Program.cs
+++ 01_Hola_Mundo_Usando_Instrucciones_De_Nivel_Superior/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //forma de crear un hola mundo en C#
 //usando instrucciones de nivel superior en C#
+// esto  es desde las versiones de  dotnet 6.0 esta disponible
 Console.WriteLine("Hello, World!");
 Console.WriteLine("\n Este mensaje es para recordarte que  fue escrito este programa" +
     " usando las instrucciones de nivel superior");
+Console.Write("\n Hola Generation");
